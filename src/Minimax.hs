4f1bdf6e05436f3ffd32bfc7a75aa491ba0742f9
Jerry <jerryzs@students.cs.ubc.ca>
2024-02-25 21:55:54 -0800

    feat(minimax): complete implementation

--- src/Minimax.hs
+++ src/Minimax.hs
@@ -1,37 +1,44 @@
 module Minimax
     ( find
+    , playAI
+    , playBest
     ) where
 
 import Game
-    ( Action (..)
-    , BigBoard
-    , Board
-    , BoardAction
-    , BoardChoice
-    , Player
-    , Square (..)
-    , State (..)
-    , checkAction
-    , draw
-    , getActions
-    , getBoardActions
-    , getBoardWinner
-    , initialState
-    , nextp
-    , play
-    , result
-    , start
-    , win
-    , winner
+
+import Data.List
+    ( sortBy
     )
 
-find :: State -> (Double, BoardAction)
-find (State sq p o)
-    | o == -1 = (0.0, 0)
-    | otherwise = (fst v, snd (head (snd v)))
-        where   v   = boardVal p (sq!!o, p, 9, [], [])
-
 type Val = (Double, [(Player, BoardAction)])
+
+find :: State -> (Double, Action)
+find (State sq p o)
+    | o == -1 = head (sortBy f [extm n (boardVal p (sq!!n, p, 5, nfv, [])) | n <- ps])
+    | otherwise = extm o val
+        where
+            ps      = [n | n <- [0..8], isSquarePlayable (sq!!n) (-1)]
+            nfv     = [n | n <- [0..8], let s = sq!!n, not (isSquarePlayable s (-1)) || abs (advantage p s) > 1]
+            val     = boardVal p (sq!!o, p, 9, nfv, [])
+            extm bc (v, pth)
+                    = (v, Action bc (snd (head pth)))
+            f (a, _) (b, _)
+                    = compare b a
+
+playBest :: State -> State
+playBest s = play (s, snd (find s))
+
+playAI :: (State, Action) -> State
+playAI (s, a) = playBest (play (s, a))
+
+advantage :: Player -> Square -> Int
+advantage _ (Win _) = 0
+advantage p (Board b) = uncurry (-) c
+    where   f x y   = case x of
+                Just a | a == p -> (fst y + 1, snd y)
+                Just _          -> (fst y, snd y + 1)
+                Nothing         -> y
+            c       = foldr f (0, 0) b
 
 boardVal :: Player -> (Square, Player, Int, [Int], [(Player, BoardAction)]) -> Val
 boardVal wp (Win p, _, _, _, _)
